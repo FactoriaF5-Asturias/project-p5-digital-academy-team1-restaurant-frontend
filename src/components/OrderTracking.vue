5d22895b09395c31e43a1c4a6d62390ee29dc7f7
andreaperezgon <andreaperezgon@gmail.com>
2026-09-10 14:51:10 +0200

    feat: add order tracking status structure

--- src/components/OrderTracking.vue
+++ src/components/OrderTracking.vue
@@ -1,24 +1,45 @@
 <script setup>
+import OrderStatusItem from './OrderStatusItem.vue'
+
 const orderStatuses = [
-    "Pedido recibido y Pagado",
-    "Cocina preparando",
-    "Listo en cocina",
-    "En Tránsito con Motorista",
-    "Entregado al destinatario"
+  {
+    name: 'Pedido recibido y Pagado',
+    time: '20:42'
+  },
+  {
+    name: 'Cocina preparando',
+    time: '20:45'
+  },
+  {
+    name: 'Listo en cocina',
+    time: '21:03'
+  },
+  {
+    name: 'En Tránsito con Motorista',
+    time: '21:08'
+  },
+  {
+    name: 'Entregado al destinatario',
+    time: null
+  }
 ]
+
+const activeStatus = 'En Tránsito con Motorista'
 </script>
 
 <template>
-    <section>
-        <h2>Estado de tu Pedido</h2>
+  <section class="page-container py-8">
+    <h2 class="mb-6 text-left">
+      Estado de tu Pedido
+    </h2>
 
-        <ul>
-            <li
-            v-for="status in orderStatuses"
-            :key="status"
-            >
-            {{ status }}
-        </li>
-        </ul>
-    </section>
+    <ul class="space-y-2">
+      <OrderStatusItem
+        v-for="status in orderStatuses"
+        :key="status.name"
+        :status="status"
+        :active-status="activeStatus"
+      />
+    </ul>
+  </section>
 </template>
