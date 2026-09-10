daee51dd7c94c1854dbb6258c5d20675cedd8742
andreaperezgon <andreaperezgon@gmail.com>
2026-09-10 12:35:45 +0200

    feat: add order tracking status structure

---
+++ src/components/OrderTracking.vue
@@ -0,0 +1,24 @@
+<script setup>
+const orderStatuses = [
+    "Pedido recibido y Pagado",
+    "Cocina preparando",
+    "Listo en cocina",
+    "En Tránsito con Motorista",
+    "Entregado al destinatario"
+]
+</script>
+
+<template>
+    <section>
+        <h2>Estado de tu Pedido</h2>
+
+        <ul>
+            <li
+            v-for="status in orderStatuses"
+            :key="status"
+            >
+            {{ status }}
+        </li>
+        </ul>
+    </section>
+</template>
