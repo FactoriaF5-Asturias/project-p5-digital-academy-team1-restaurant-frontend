36b54c56daead5c634c4e162e035a11da027d64e
andreaperezgon <andreaperezgon@gmail.com>
2026-09-11 08:37:07 +0200

    Create RiderInfo

---
+++ src/components/RiderInfo.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  rider: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <article class="card flex items-center gap-4 p-4">
+    <div class="flex-1 text-left">
+      <span class="label-eyebrow text-primary">
+        Motorista asignado
+      </span>
+
+      <h3 class="mt-1">
+        {{ rider.name }}
+      </h3>
+
+      <p class="mt-1 text-on-surface-variant">
+        {{ rider.vehicle }}
+      </p>
+
+      <span class="label-code text-on-surface-variant">
+        Rider ID: {{ rider.id }}
+      </span>
+    </div>
+  </article>
+</template>
