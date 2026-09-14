7aea2068b97bfb6fb4a1874113f7463d14d4a782
Durán <nievesyjonathan23@gmail.com>
2026-09-14 10:22:41 +0200

    feat: add quick suggestion phrases for the chef note

---
+++ src/constants/chefNoteSuggestions.js
@@ -0,0 +1,9 @@
+// src/constants/chefNoteSuggestions.js
+
+export const CHEF_NOTE_SUGGESTIONS = Object.freeze([
+  'Sin wasabi',
+  'Palillos extra',
+  'Sin gluten',
+  'Alergia a frutos secos',
+  'Sin picante',
+])
