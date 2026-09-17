27596c0459b862b9aab12e648ff36913c3e21b06
andreaperezgon <andreaperezgon@gmail.com>
2026-09-17 11:04:28 +0200

    feat: integrar ticket en la vista de mi pedido

--- src/views/MiPedidoView.vue
+++ src/views/MiPedidoView.vue
@@ -1,9 +1,35 @@
 <script setup>
-import OrderTracking from '../components/OrderTracking.vue';
+import OrderTracking from '../components/OrderTracking.vue'
+import OrderTicket from '../components/OrderTicket.vue'
+
+const order = {
+  orderNumber: 'GS-2026-00125',
+  paymentStatus: 'Pagado',
+  items: [
+    {
+      id: 1,
+      name: 'Pull Nigiri',
+      quantity: 2,
+      price: 6.5,
+    },
+    {
+      id: 2,
+      name: 'Merge Maki',
+      quantity: 1,
+      price: 8.9,
+    },
+  ],
+}
 </script>
 
 <template>
   <main>
     <OrderTracking />
+
+    <OrderTicket
+      :order-number="order.orderNumber"
+      :payment-status="order.paymentStatus"
+      :items="order.items"
+    />
   </main>
 </template>
