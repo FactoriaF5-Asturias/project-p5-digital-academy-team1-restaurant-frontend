16e33b5a0e4bc9074ba88fb6ae6211d87548ad73
andreaperezgon <andreaperezgon@gmail.com>
2026-09-17 10:01:14 +0200

    feat: mostrar informacion principal del ticket

---
+++ src/components/OrderTicket.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  orderNumber: {
+    type: String,
+    required: true,
+  },
+  paymentStatus: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <section>
+    <h2>Ticket Manifest</h2>
+
+    <div>
+      <p>Pedido #{{ orderNumber }}</p>
+      <p>Estado del pago: {{ paymentStatus }}</p>
+    </div>
+  </section>
+</template>
