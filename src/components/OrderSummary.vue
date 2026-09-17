6f7ebb7a2922186f2be270272fe7f55ff036517d
andreaperezgon <andreaperezgon@gmail.com>
2026-09-17 11:15:09 +0200

    feat: display order payment breakdown

--- src/components/OrderSummary.vue
+++ src/components/OrderSummary.vue
@@ -1,5 +1,32 @@
+<script setup>
+defineProps({
+  subtotal: {
+    type: Number,
+    required: true,
+  },
+  deliveryFee: {
+    type: Number,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+})
+</script>
 <template>
   <section>
     <h3>Resumen del pago</h3>
+
+    <div>
+      <p>Subtotal: {{ subtotal }} €</p>
+      <p>Gastos de entrega: {{ deliveryFee }} €</p>
+      <p>Total abonado: {{ total }} €</p>
+      <p>Método de pago: {{ paymentMethod }}</p>
+    </div>
   </section>
-</template> 
+</template>
