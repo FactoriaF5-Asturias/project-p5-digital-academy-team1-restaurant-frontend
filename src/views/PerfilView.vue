b52e332d0da259f78e158ff60f5a8271acbd6d9d
Durán <nievesyjonathan23@gmail.com>
2026-09-25 11:32:06 +0200

    feat: build profile view with exclusive offers card

--- src/views/PerfilView.vue
+++ src/views/PerfilView.vue
@@ -1,3 +1,23 @@
+<script setup>
+import ExclusiveOffersCard from "../components/ExclusiveOffersCard.vue";
+</script>
+
 <template>
-  <div>Perfil</div>
+  <main class="perfil-view">
+    <h1 class="perfil-view__title">Mi Perfil</h1>
+
+    <ExclusiveOffersCard />
+  </main>
 </template>
+
+<style scoped>
+@reference "../style.css";
+
+.perfil-view {
+  @apply flex flex-col gap-6 max-w-3x1 mx-auto px-4 sm:px-6 py-8;
+}
+
+.perfil-view-title {
+  @apply text-2xl font-heading;
+}
+</style>
