ae8edb90eb4cba092a5b39ee6d94e04b1cad08e4
Durán <nievesyjonathan23@gmail.com>
2026-09-25 10:40:25 +0200

    feat: mount order history section in order view

--- src/views/MiPedidoView.vue
+++ src/views/MiPedidoView.vue
@@ -1,43 +1,46 @@
 <script setup>
-import OrderTracking from '../components/OrderTracking.vue'
-import OrderTicket from '../components/OrderTicket.vue'
+import OrderTracking from "../components/OrderTracking.vue";
+import OrderTicket from "../components/OrderTicket.vue";
+import OrderHistorySection from "../components/OrderHistorySection.vue";
 
 const order = {
-  orderNumber: 'GS-2026-00125',
-  paymentStatus: 'Pagado',
+  orderNumber: "GS-2026-00125",
+  paymentStatus: "Pagado",
   items: [
     {
       id: 1,
-      name: 'Pull Nigiri',
+      name: "Pull Nigiri",
       quantity: 2,
       price: 6.5,
     },
     {
       id: 2,
-      name: 'Merge Maki',
+      name: "Merge Maki",
       quantity: 1,
       price: 8.9,
     },
   ],
-  subtotal: 21.90,
-deliveryFee: 2.50,
-total: 24.40,
-paymentMethod: 'Tarjeta',
-}
+  subtotal: 21.9,
+  deliveryFee: 2.5,
+  total: 24.4,
+  paymentMethod: "Tarjeta",
+};
 </script>
 
 <template>
   <main>
     <OrderTracking />
 
-  <OrderTicket
-  :order-number="order.orderNumber"
-  :payment-status="order.paymentStatus"
-  :items="order.items"
-  :subtotal="order.subtotal"
-  :delivery-fee="order.deliveryFee"
-  :total="order.total"
-  :payment-method="order.paymentMethod"
-/>
+    <OrderTicket
+      :order-number="order.orderNumber"
+      :payment-status="order.paymentStatus"
+      :items="order.items"
+      :subtotal="order.subtotal"
+      :delivery-fee="order.deliveryFee"
+      :total="order.total"
+      :payment-method="order.paymentMethod"
+    />
+
+    <OrderHistorySection />
   </main>
 </template>
