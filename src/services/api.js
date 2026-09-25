f03659a9020f307e1fe9c79125ce70cbf10dedd3
Danyil Muntianu <danielmuntyanu@gmail.com>
2026-09-25 14:26:19 +0200

    feat (GS-661): Configured CSRF and prepared for Cookie based JWT authentication

--- src/services/api.js
+++ src/services/api.js
@@ -2,15 +2,9 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-})
-
-// Interceptor: añade el token de sesión a cada petición si existe
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config
+  withCredentials: true,
+  xsrfCookieName: 'XSRF-TOKEN',
+  xsrfHeaderName: 'X-XSRF-TOKEN',
 })
 
 export default api
